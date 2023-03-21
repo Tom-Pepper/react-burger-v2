d18be37bc5e543c3e8e6045841053b594fa6815f
Artem Kondratyev <walinor.88@gmail.com>
2023-03-21 20:07:12 +0300

    feat: add cart calculation layout

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './burger-constructor.module.css';
+import {Button, CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import ConstructorCard from "../constructor-card/constructor-card";
 import {testData} from "../../utils/data";
 
@@ -17,13 +18,28 @@ function BurgerConstructor() {
   }
 
   return (
-    <div className="mt-25">
-      <ConstructorCard type="bun-top" item={testData[0]}/>
-      <ul className={styles.constructor__list}>
-        {renderConstructor()}
-      </ul>
-      <ConstructorCard type="bun-bottom" item={testData[0]}/>
-    </div>
+    <>
+      <div className="mt-25 mb-10">
+        <ConstructorCard type="bun-top" item={testData[0]}/>
+        <ul className={styles.constructor__list}>
+          {renderConstructor()}
+        </ul>
+        <ConstructorCard type="bun-bottom" item={testData[0]}/>
+      </div>
+      <div className={styles.constructor__wrap}>
+        <div className={styles.constructor__wrap}>
+          <p className="text text_type_digits-medium">
+            1234
+          </p>
+          <span className={styles.constructor__icon}>
+            <CurrencyIcon type="primary" />
+          </span>
+        </div>
+        <Button htmlType="button" type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
+    </>
   );
 }
 
